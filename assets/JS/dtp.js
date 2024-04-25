fa43231c15715e05f973f7ccf2199e6da6b39046
Khamadullin <144838600+Khamadullin@users.noreply.github.com>
2024-04-26 02:33:44 +0300

    Add files via upload

---
+++ assets/JS/dtp.js
@@ -0,0 +1,29 @@
+function newTextAllDtp() {
+    var newText = document.getElementById('text-input-dtp-all').value;
+    document.getElementById('dtp-all').innerHTML = newText;
+}
+
+function newTextInSity() {
+    var newText = document.getElementById('text-input-dtp-city').value;
+    document.getElementById('dtp-city').innerHTML = newText;
+}
+
+function newTextSubject() {
+    var newText = document.getElementById('text-input-dtp-subject').value;
+    document.getElementById('dtp-subject').innerHTML = newText;
+}
+
+function newTextRoad() {
+    var newText = document.getElementById('text-input-dtp-road').value;
+    document.getElementById('dtp-road').innerHTML = newText;
+}
+
+function newTextPostrad() {
+    var newText = document.getElementById('text-input-dtp-postrad').value;
+    document.getElementById('dtp-postrad').innerHTML = newText;
+}
+
+function newTextPogib() {
+    var newText = document.getElementById('text-input-dtp-pogib').value;
+    document.getElementById('dtp-pogib').innerHTML = newText;
+}
